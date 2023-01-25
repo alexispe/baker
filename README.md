84f6bc19032bccaa3d6b2aa3c6909226e136b777
Alexis <contact@alexis-petit.fr>
2023-01-25 22:06:32 +0100

    Migrate bash script to PHP

--- README.md
+++ README.md
@@ -1,35 +1,39 @@
-# Baker
+# Baker - A simple PHP backup script
 
-Baker is a simple and effective bash script that allows you to automate the backups of your websites and databases. It also sends you a notification on Slack when the backup is complete.
+Baker is a simple and effective php script that allows you to automate the backups of your websites and databases. 
+It also sends you a notification on Slack when the backup is complete.
 
 ## Prerequisites
 
-Before you can use Baker, you need to have the following items installed:
+Before you can use Baker, you need to have the following items:
 
-- bash
-- curl (for sending HTTP requests)
+- PHP 5.2 or higher
+- PHP curl extension
+- PECL yaml extension
+
+Usually, these are already installed on your server.
 
 ## Installation
 
 To use Baker, follow these steps:
 
-1. Clone this repository to your server.
-2. Open the `baker.config` file in your preferred text editor and modify the following variables by inserting your own values:
+1. Copy the `baker.php` and `baker.yaml` files to your server.
+2. Open the `baker.yaml` file and modify the following variables by inserting your own values:
    - `slack_webhook_url`: the URL of the Slack webhook you want to use to receive notifications.
    - `site_url`: the URL of the website you want to back up. This is used to generate the name of the backup file.
    - `site_path`: the path to the website's root directory.
-   - `db_name`: the name of the database to be backed up.
-   - `db_host`: the host of the database (e.g. localhost).
-   - `db_user`: the username of the database.
-   - `db_pass`: the password of the database user.
-   - `db_port`: the port of the database.
-   - `ftp_host`: the host of the FTP server.
-   - `ftp_user`: the username of the FTP server.
-   - `ftp_pass`: the password of the FTP server user.
+   - `db.name`: the name of the database to be backed up.
+   - `db.host`: the host of the database (e.g. localhost).
+   - `db.user`: the username of the database.
+   - `db.pass`: the password of the database user.
+   - `db.port`: the port of the database.
+   - `ftp.host`: the host of the FTP server.
+   - `ftp.user`: the username of the FTP server.
+   - `ftp.pass`: the password of the FTP server user.
 
 ## Usage
 
-To run a backup of your website and database with Baker, simply run the script using the `bash baker.sh` command. 
+To run a backup of your website and database with Baker, simply run the script using the `php baker.php` command. 
 The backup will be performed and a notification will be sent to Slack when it is complete.
 
 You can also use a task scheduler (such as `cron`) to automate the backup on a regular interval.
---
+++ baker.php
@@ -0,0 +1,152 @@
+#!/usr/bin/php
+<?php
+
+try {
+    echo "Get settings from baker.yaml" . PHP_EOL;
+    $settings = yaml_parse_file('baker.yaml');
+    $siteUrl = $settings['site_url'];
+    $slackWebhookUrl = $settings['slack_webhook_url'];
+    $sitePath = $settings['site_path'];
+    $dbName = $settings['database']['name'];
+    $dbHost = $settings['database']['host'];
+    $dbUser = $settings['database']['user'];
+    $dbPass = $settings['database']['pass'];
+    $dbPort = $settings['database']['port'];
+    $ftpHost = $settings['ftp']['host'];
+    $ftpUser = $settings['ftp']['user'];
+    $ftpPass = $settings['ftp']['pass'];
+
+    $archiveName = 'backup-' . $siteUrl . '-' . date('Y-m-d-H-i-s') . '.tar.gz';
+    $tempDir = 'tmp-backup-' . $siteUrl . '-' . date('Y-m-d-H-i-s');
+
+    echo "Creating temporary directory $tempDir" . PHP_EOL;
+    mkdir($tempDir);
+
+    echo "Compressing site files" . PHP_EOL;
+    exec("tar -czf $tempDir/site.tar.gz $sitePath");
+
+    echo "Creating database dump" . PHP_EOL;
+    exec("mysqldump --host=$dbHost --user=$dbUser --password=$dbPass --port=$dbPort $dbName --no-tablespaces > $tempDir/db.sql");
+
+    echo "Creating archive $archiveName" . PHP_EOL;
+    exec("tar -czf $archiveName $tempDir");
+
+    $backupSize = filesize($archiveName);
+
+    echo "Removing temporary directory $tempDir" . PHP_EOL;
+    exec("rm -rf $tempDir");
+
+    echo "Uploading archive to FTP" . PHP_EOL;
+    $ftpConnection = ftp_connect($ftpHost);
+    ftp_login($ftpConnection, $ftpUser, $ftpPass);
+    ftp_pasv($ftpConnection, true);
+    ftp_put($ftpConnection, $archiveName, $archiveName, FTP_BINARY);
+    ftp_close($ftpConnection);
+
+    echo "Removing archive $archiveName" . PHP_EOL;
+    exec("rm -rf $archiveName");
+
+    echo "Removing backups older than 7 days" . PHP_EOL;
+    $backupRemovedCount = 0;
+    $ftpConnection = ftp_connect($ftpHost);
+    ftp_login($ftpConnection, $ftpUser, $ftpPass);
+    ftp_pasv($ftpConnection, true);
+    $files = ftp_nlist($ftpConnection, '.');
+    foreach ($files as $file) {
+        if (strpos($file, 'backup-' . $siteUrl) !== false) {
+            $fileTime = ftp_mdtm($ftpConnection, $file);
+            if ($fileTime < strtotime('-7 days')) {
+                ftp_delete($ftpConnection, $file);
+                $backupRemovedCount++;
+            }
+        }
+    }
+    ftp_close($ftpConnection);
+
+    echo "Sending notification to Slack" . PHP_EOL;
+    curl_setopt_array($curl = curl_init(), [
+        CURLOPT_URL => $slackWebhookUrl,
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => json_encode([
+            'text' => "Backup of $siteUrl has been created",
+            'attachments' => [
+                [
+                    'color' => '#36a64f',
+                    'fields' => [
+                        [
+                            'title' => 'Site URL',
+                            'value' => $siteUrl,
+                            'short' => true,
+                        ],
+                        [
+                            'title' => 'Date',
+                            'value' => date('Y-m-d H:i:s'),
+                            'short' => true,
+                        ],
+                        [
+                            'title' => 'Size',
+                            'value' => human_filesize($backupSize),
+                            'short' => true,
+                        ],
+                        [
+                            'title' => 'Backups removed',
+                            'value' => $backupRemovedCount,
+                            'short' => true,
+                        ],
+                    ],
+                ],
+            ],
+        ]),
+        CURLOPT_HTTPHEADER => [
+            'Content-Type: application/json',
+        ],
+    ]);
+    curl_exec($curl);
+    curl_close($curl);
+}
+catch (Exception $e) {
+    echo "Sending error notification to Slack" . PHP_EOL;
+    curl_setopt_array($curl = curl_init(), [
+        CURLOPT_URL => $slackWebhookUrl,
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => json_encode([
+            'text' => "Backup of $siteUrl has failed",
+            'attachments' => [
+                [
+                    'color' => '#ff0000',
+                    'fields' => [
+                        [
+                            'title' => 'Site URL',
+                            'value' => $siteUrl,
+                            'short' => true,
+                        ],
+                        [
+                            'title' => 'Date',
+                            'value' => date('Y-m-d H:i:s'),
+                            'short' => true,
+                        ],
+                        [
+                            'title' => 'Error',
+                            'value' => $e->getMessage(),
+                            'short' => false,
+                        ],
+                    ],
+                ],
+            ],
+        ]),
+        CURLOPT_HTTPHEADER => [
+            'Content-Type: application/json',
+        ],
+    ]);
+    curl_exec($curl);
+    curl_close($curl);
+}
+
+
+function human_filesize($bytes, $dec = 2)
+{
+    $size   = array('B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
+    $factor = floor((strlen($bytes) - 1) / 3);
+
+    return sprintf("%.{$dec}f", $bytes / pow(1024, $factor)) . @$size[$factor];
+}
